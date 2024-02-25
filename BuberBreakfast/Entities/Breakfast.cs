3291dd22e4c407eb4f9bebccf1f2b62c17ac9421
lion772 <williamlopes64@gmail.com>
2024-02-25 14:25:48 +0100

    Add reference to BuberBreakfast.Contracts, create BreakfastsController.cs and handle all Http requests  in it.

---
+++ BuberBreakfast/Entities/Breakfast.cs
@@ -0,0 +1,10 @@
+namespace BuberBreakfast.Entities;
+
+public class Breakfast {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public List<string> Savory { get; set; }
+        public List<string> Sweet { get; set; }
+}
