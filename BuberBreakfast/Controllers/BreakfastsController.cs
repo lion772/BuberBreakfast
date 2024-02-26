685a3a9156b51c820eaa259528733bf0a341a3ac
lion772 <williamlopes64@gmail.com>
2024-02-26 16:21:11 +0100

    add BreakfastService that implements IBreakfastService, in which create and get methods have been added.

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -1,61 +1,71 @@
 using BuberBreakfast.Contracts.Breakfast;
 using Microsoft.AspNetCore.Mvc;
 using BuberBreakfast.Models;
+using BuberBreakfast.Services.Breakfasts;
 
 namespace BuberBreakfast.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class BreakfastsController: ControllerBase 
 {
-        [HttpPost()]
-        public IActionResult CreateBreakfast([FromBody] CreateBreakfastRequest request)
-        {
-                var breakfast = new Breakfast (
-                        Guid.NewGuid(),
-                        request.Name,
-                        request.Description,
-                        request.StartDateTime,
-                        request.EndDateTime,
-                        DateTime.UtcNow,
-                        request.Savory,
-                        request.Sweet
-                ); 
-               //TODO: save breakfast to database
+     private readonly IBreakfastService _breakfastService;
 
-               var respose = new BreakfastResponse(
-                    breakfast.Id,
-                    breakfast.Name,
-                    breakfast.Description,
-                    breakfast.StartDateTime,
-                    breakfast.EndDateTime,
-                    breakfast.LastModifiedDateTime,
-                    breakfast.Savory,
-                    breakfast.Sweet
-               );
+     public BreakfastsController(IBreakfastService breakfastService)
+     {
+          _breakfastService = breakfastService;
+     }
+     
+     [HttpPost()]
+     public IActionResult CreateBreakfast([FromBody] CreateBreakfastRequest request)
+     {
+               var breakfast = new Breakfast (
+                    Guid.NewGuid(),
+                    request.Name,
+                    request.Description,
+                    request.StartDateTime,
+                    request.EndDateTime,
+                    DateTime.UtcNow,
+                    request.Savory,
+                    request.Sweet
+               ); 
+          //TODO: save breakfast to database
+          _breakfastService.CreateBreakfast(breakfast);
 
-               return CreatedAtAction(
-                    actionName: nameof(GetBreakfast),
-                    routeValues: new { id = breakfast.Id },
-                    respose);           
+          var respose = new BreakfastResponse(
+               breakfast.Id,
+               breakfast.Name,
+               breakfast.Description,
+               breakfast.StartDateTime,
+               breakfast.EndDateTime,
+               breakfast.LastModifiedDateTime,
+               breakfast.Savory,
+               breakfast.Sweet
+          );
+
+          return CreatedAtAction(
+               actionName: nameof(GetBreakfast),
+               routeValues: new { id = breakfast.Id },
+               respose);           
 
 
-        }
+     }
 
-        [HttpGet("{id:guid}")]
-        public IActionResult GetBreakfast(Guid id)
-        {
-             return Ok(id);           
-        }
-        
-        [HttpPut("{id:guid}")]
-        public IActionResult UpsertBreakfast(Guid id, [FromBody] UpsertBreakfastRequest request)
-        {
-             return Ok(request);           
-        }
+     [HttpGet("{id:guid}")]
+     public IActionResult GetBreakfast(Guid id)
+     {
+          _breakfastService.GetBreakfast(id);
+          return Ok(id);           
+     }
+     
+     [HttpPut("{id:guid}")]
+     public IActionResult UpsertBreakfast(Guid id, [FromBody] UpsertBreakfastRequest request)
+     {
+          return Ok(request);           
+     }
 
-        [HttpDelete("{id:guid}")]
-        public IActionResult DeleteBreakfast(Guid id)
-        {
-             return Ok(id);           
-        }
+     [HttpDelete("{id:guid}")]
+     public IActionResult DeleteBreakfast(Guid id)
+     {
+          return Ok(id);           
+     }
 }
--- BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
+++ BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d46668cd1168563e2f367528dd6a4154361d347")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c51dfbfadb7d568ae01c7dc3a6fad97f331c07")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
